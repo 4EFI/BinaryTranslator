ff5f5d7573beaf795c594023f85a5d70985ad19e
4EFI <kvv204@gmail.com>
2023-04-30 16:50:44 +0300

    Update makefile

--- LngHandler/bin_translating_x86/src/main.cpp
+++ LngHandler/bin_translating_x86/src/main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "commands.h"
+// #include "commands.h"
 #include "bin_trtor.h"
 
 //-----------------------------------------------------------------------------
